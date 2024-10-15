5571dd11c69d2b3b1ff307c8b97893169a03448c
Goossens Levi <Levi.Goossens@student.pxl.be>
2024-10-15 15:13:35 +0200

    oplossing in de klas gemaakt

---
+++ Priemgetallen/Program.cs
@@ -0,0 +1,23 @@
+﻿for (int number = 1; number <= 1000; number++)
+{
+    bool isPrime = true;
+
+    for (int check = 2;  check <= (number/2); check++)
+    {
+        if (number % check == 0)
+            isPrime = false;
+    }
+
+    if (isPrime && number != 1)
+        Console.ForegroundColor = ConsoleColor.Red;
+    else 
+        Console.ForegroundColor = ConsoleColor.Green;
+
+    Console.Write($"{number}\t");
+    Console.ResetColor();
+
+    if ( number % 10 == 0 )
+    {
+        Console.WriteLine();
+    }
+}
